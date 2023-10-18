52f0bba700366e0d3ca09674698593f951afef95
Tiago Godinho <jtmgodinho@gmail.com>
2023-10-18 10:41:17 +0100

    Adding stretch goals to challenge 07

--- challenge class 07/os-walk.py
+++ challenge class 07/os-walk.py
@@ -2,9 +2,10 @@
 
 # Import libraries
 import os
+import subprocess
 
 ### Declare a function here
-def dir_structure(directory_path):
+def dir_structure_print(directory_path):
     for (root, dirs, files) in os.walk(directory_path):
         print(f"==root==")
         print(root)
@@ -13,9 +14,23 @@ def dir_structure(directory_path):
         print(f"==files==")
         print(files)
 
+def dir_structure_output(directory_path):
+    with open("dir_structure.txt", "w") as output_file:
+        for (root, dirs, files) in os.walk(directory_path):
+            output_file.write("==root==\n")
+            output_file.write(root + "\n")
+            output_file.write("==dirs==\n")
+            output_file.write(", ".join(dirs) + "\n")
+            output_file.write("==files==\n")
+            output_file.write(", ".join(files) + "\n")
+    print("Directory structure saved to 'dir_structure.txt'")
+    # Open the text file with LibreOffice Writer
+    subprocess.Popen(["libreoffice", "--writer", "dir_structure.txt"])
+
 # Main
 def main():
     user_input = input("Enter a directory path: ")
-    dir_structure(user_input)
+    dir_structure_print(user_input)
+    dir_structure_output(user_input)
 
 main()
